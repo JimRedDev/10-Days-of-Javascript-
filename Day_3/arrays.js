435a7e882255bd0d27b4a9f556500b4ec9f148b3
James Bowler <jimuk07@yahoo.co.uk>
2018-10-25 23:17:01 +0100

    array.js modified

--- Day_3/arrays.js
+++ Day_3/arrays.js
@@ -1,8 +1,8 @@
-/**
+/*
 *   Return the second largest number in the array.
 *   @param {Number[]} nums - An array of numbers.
 *   @return {Number} The second largest number in the array.
-**/
+*/
 
 
 function getSecondLargest(nums) {
