1dfc6caadd90e3c79437f235308a100dbf61fab0
James Bowler <jimuk07@yahoo.co.uk>
2018-10-28 18:20:45 +0000

    day4 complete

---
+++ Day_4/classes.js
@@ -0,0 +1,17 @@
+/*
+ * Implement a Polygon class with the following properties:
+ * 1. A constructor that takes an array of integer side lengths.
+ * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+ */
+
+
+ 
+class Polygon{
+    constructor(arr) {
+        this._sides = arr; 
+    }
+    
+    perimeter() {
+        return this._sides.reduce((accumulator, currentValue) => accumulator + currentValue);
+    }
+}
